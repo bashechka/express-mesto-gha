bd2f7c9c4d3da2fbb8be3060df74ccf207973942
Varvara Korobkina <ya.bashechka@yandex.ru>
2023-02-06 12:58:31 +0700

    Fix validation

---
+++ validators/validators.js
@@ -0,0 +1,17 @@
+const { Joi, celebrate } = require('celebrate');
+
+module.exports.signUpValidator = celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    email: Joi.string().email().required(),
+    password: Joi.string().required(),
+  }),
+});
+
+module.exports.signInValidator = celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().email().required(),
+    password: Joi.string().required(),
+  }),
+});
